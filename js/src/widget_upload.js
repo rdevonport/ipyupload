31b5a9ddbd451dde050e684b8ecca86ad714e4c9
oscar6echo <olivier.borderies@gmail.com>
2018-10-29 00:37:05 +0100

    Update

--- js/src/widget_upload.js
+++ js/src/widget_upload.js
@@ -1,7 +1,10 @@
 import * as widgets from '@jupyter-widgets/base';
 import { extend } from 'lodash';
+import '@jupyter-widgets/controls/css/widgets.css';
+
 import { version } from '../package.json';
 import * as Utils from './widget_utils';
+import './widget_style.css';
 
 const semver_range = `~${version}`;
 
@@ -43,7 +46,7 @@ const FileUploadView = widgets.DOMWidgetView.extend({
     render() {
         const that = this;
         that._id = this.model.get('_id');
-        that._counter = this.model.get('_counter');
+        that.counter = this.model.get('_counter');
 
         const divWidget = this.el;
         const divNew = document.createElement('div');
@@ -53,23 +56,29 @@ const FileUploadView = widgets.DOMWidgetView.extend({
 
         const fileInput = document.createElement('input');
         fileInput.type = 'file';
-        fileInput.multiple = true;
+        fileInput.multiple = that.model.get('multiple');
+        fileInput.className = 'jupyter-widget-file-upload';
+        fileInput.id = `file_upload-${that._id}`;
         divNew.appendChild(fileInput);
+
+        const label = document.createElement('label');
+        label.htmlFor = `file_upload-${that._id}`;
+        label.innerHTML = '<i class="fa fa-upload"></i>  Upload';
+        label.className = 'p-Widget jupyter-widgets jupyter-button widget-button';
+        divNew.appendChild(label);
 
         window.fileInput = fileInput;
 
-        fileInput.addEventListener('click', function(e) {
+        fileInput.addEventListener('click', () => {
             fileInput.value = '';
         });
 
-        fileInput.addEventListener('change', function(e) {
+        fileInput.addEventListener('change', () => {
             console.log('new input');
             window.fileInput = fileInput;
-            console.log('nb files = ' + fileInput.files.length);
-
+            console.log(`nb files = ${fileInput.files.length}`);
             const promisesFile = [];
-
-            for (let file of fileInput.files) {
+            Array.from(fileInput.files).map(file => {
                 console.log(file);
                 promisesFile.push(
                     new Promise((resolve, reject) => {
@@ -83,14 +92,10 @@ const FileUploadView = widgets.DOMWidgetView.extend({
                         };
                         const fileReader = new FileReader();
                         fileReader.onload = event => {
-                            // let buffer = fileReader.result;
-                            let buffer = event.target.result;
-                            // let view = new DataView(buffer, 0, buffer.byteLength);
-                            // let view = new DataView(buffer.slice(0));
+                            const buffer = event.target.result;
                             resolve({
-                                buffer: buffer,
-                                // view: view,
-                                metadata: metadata,
+                                buffer,
+                                metadata,
                             });
                         };
                         fileReader.onerror = () => {
@@ -101,35 +106,32 @@ const FileUploadView = widgets.DOMWidgetView.extend({
                         fileReader.readAsArrayBuffer(file);
                     })
                 );
-            }
-
+            });
             Promise.all(promisesFile)
                 .then(contents => {
                     window.contents = contents;
                     console.log(contents);
                     const li_metadata = [];
                     const li_buffer = [];
-                    for (let c of contents) {
+                    contents.map(c => {
                         console.log(c);
-                        console.log(c.metadata);
-                        console.log(c.buffer);
-                        let arrayInt8 = new Int8Array(c.buffer);
-                        let arrayUInt8 = new Uint8Array(c.buffer);
-                        console.log(JSON.stringify(arrayInt8));
-                        console.log(JSON.stringify(arrayUInt8));
+                        // console.log(c.metadata);
+                        // console.log(c.buffer);
+                        // let arrayInt8 = new Int8Array(c.buffer);
+                        // let arrayUInt8 = new Uint8Array(c.buffer);
+                        // console.log(JSON.stringify(arrayInt8));
+                        // console.log(JSON.stringify(arrayUInt8));
                         // let view = c.view;
                         // console.log(view);
                         li_metadata.push(c.metadata);
                         li_buffer.push(c.buffer);
-                    }
+                    });
                     Utils.show('li_metadata', li_metadata);
                     Utils.show('li_buffer', li_buffer);
-
-                    that._counter += 1;
-
+                    that.counter += 1;
                     that.model.set({
-                        _counter: that._counter,
-                        li_metadata: li_metadata,
+                        _counter: that.counter,
+                        li_metadata,
                         li_content: li_buffer,
                         error: '',
                     });
@@ -137,10 +139,9 @@ const FileUploadView = widgets.DOMWidgetView.extend({
                 })
                 .catch(err => {
                     console.error('error in file upload: %o', err);
-                    that._counter += 1;
-
+                    that.counter += 1;
                     that.model.set({
-                        _counter: that._counter,
+                        _counter: that.counter,
                         error: err,
                     });
                     that.touch();
