15d719244503f2c01b96704cab1ea2150ae88e56
rdevonport <rory.devonport@gmail.com>
2019-04-15 16:20:10 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,73 +1,19 @@
-
-# necessary to push to PyPI
-# cf. http://peterdowns.com/posts/first-time-with-pypi.html
-# cf. https://tom-christie.gitlab.io/articles/pypi/
-# cf. https://pythonhosted.org/setuptools/setuptools.html
-
-# commands:
-# python setup.py sdist
-# python setup.py bdist_wheel
-# twine upload dist/*
-
-
-from distutils.util import convert_path
 from setuptools import setup, find_packages
 
-packages = find_packages()
-module = packages[0]
-print('module = {}'.format(module))
+data_files = [
+    ('share/jupyter/nbextensions/ipyupload', [
+        'ipyupload/static/extension.js',
+        'ipyupload/static/index.js',
+    ]),
+    # classic notebook extension
+    ('etc/jupyter/nbconfig/notebook.d', [
+        'ipyupload/ipyupload.json'
+    ]),
+]
 
-meta_ns = {}
-ver_path = convert_path(module + '/__meta__.py')
-with open(ver_path) as ver_file:
-    exec(ver_file.read(), meta_ns)
-
-name = meta_ns['__name__']
-packages = packages
-version = meta_ns['__version__']
-description = meta_ns['__description__']
-long_description = meta_ns['__long_description__']
-author = meta_ns['__author__']
-author_email = meta_ns['__author_email__']
-url = meta_ns['__url__']
-download_url = meta_ns['__download_url__']
-keywords = meta_ns['__keywords__']
-license = meta_ns['__license__']
-classifiers = meta_ns['__classifiers__']
-include_package_data = meta_ns['__include_package_data__']
-package_data = meta_ns['__package_data__']
-data_files = meta_ns['__data_files__']
-zip_safe = meta_ns['__zip_safe__']
-entry_points = meta_ns['__entry_points__']
-
-
-# read requirements.txt
-with open('requirements.txt', 'r') as f:
-    content = f.read()
-li_req = content.split('\n')
-install_requires = [e.strip() for e in li_req if len(e)]
-
-# with open('README.md') as f:
-#     long_description = f.read()
-
-
-setup(
-    name=name,
-    version=version,
-    description=description,
-    long_description=long_description,
-    author=author,
-    author_email=author_email,
-    url=url,
-    download_url=download_url,
-    keywords=keywords,
-    license=license,
-    classifiers=classifiers,
-    packages=packages,
-    install_requires=install_requires,
-    include_package_data=include_package_data,
-    package_data=package_data,
-    data_files=data_files,
-    zip_safe=zip_safe,
-    entry_points=entry_points
-)
+setup(name='ipyupload',
+      version="0.0.1",
+      packages=find_packages("."),
+      install_requires=['ipywidgets'],
+      data_files=data_files,
+      include_package_data=True)
